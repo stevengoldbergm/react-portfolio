726b9b02f7927a1fe6bbaa166bc78e8866fd93d0
SG_Demi <stevengoldbergm@gmail.com>
2022-10-19 01:48:45 -0500

    Added About Me text. Removed redundant title

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div class="container pt-3">
-      <h1 class="title">About Page</h1>
-      <p>
-        This is where I will write an "About Me" section
+    <div class="container d-flex flex-column col-12 col-sm-7 p-3">
+      <p class="subtitle">
+        Hi there! 
+        <br></br>
+        <br></br> 
+        My name is Steve, and I am a (soon to be) certified full-stack developer. 
+        <br></br>
+        <br></br> 
+        I have a varied background, a bachelor's degree in psychology, and nearly a decade's worth of experience managing teams and projects in imaging operations for financial institutions. My passion however, lies in working with technology and coding. 
+        <br></br>
+        <br></br> 
+        Please take a look at my work, and feel free to contact me if you're looking for a developer with good communication skills, a growth mindset, and a strong work ethic!
       </p>
+
     </div>
   );
 }
