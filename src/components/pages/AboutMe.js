8da299c204881589818c5629e3ab1bb86b87591e
SG_Demi <stevengoldbergm@gmail.com>
2022-10-19 20:18:44 -0500

    Updated wording on AboutMe section

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -11,7 +11,7 @@ export default function About() {
         My name is Steve, and I am a (soon to be) certified full-stack developer. 
         <br></br>
         <br></br> 
-        I have a varied background, a bachelor's degree in psychology, and nearly a decade's worth of experience managing teams and projects in imaging operations for financial institutions. My passion however, lies in working with technology and coding. 
+        I have a varied background, a bachelor's degree in psychology, and nearly a decade's worth of experience leading teams and projects in imaging operations for financial institutions. My passion however, lies in working with technology and coding. 
         <br></br>
         <br></br> 
         Please take a look at my work, and feel free to contact me if you're looking for a developer with good communication skills, a growth mindset, and a strong work ethic!
