646fc72eaf1e5f320528ccd80ff217dd9eefeec5
SG_Demi <stevengoldbergm@gmail.com>
2022-10-19 21:46:39 -0500

    Updated syntax for REACT

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
-function NavTabs({ currentPage, handlePageChange }) {
+function Footer({ currentPage, handlePageChange }) {
   return (
   <>
-    <footer class="py-3 my-4">
-      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="https://github.com/" class="nav-link px-2 text-muted">Github</a></li>
-        <li class="nav-item"><a href="https://LinkedIn.com/" class="nav-link px-2 text-muted">LinkedIn</a></li>
-        <li class="nav-item"><a href="https://Twitter.com/" class="nav-link px-2 text-muted">Twitter</a></li>
+    <footer className="py-3 my-4">
+      <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+        <li className="nav-item"><a href="https://github.com/" className="nav-link px-2 text-muted">Github</a></li>
+        <li className="nav-item"><a href="https://LinkedIn.com/" className="nav-link px-2 text-muted">LinkedIn</a></li>
+        <li className="nav-item"><a href="https://Twitter.com/" className="nav-link px-2 text-muted">Twitter</a></li>
 
       </ul>
-      <p class="text-center text-muted">© 2022 stevengoldbergm</p>
+      <p className="text-center text-muted">© 2022 stevengoldbergm</p>
     </footer>
   </>
   );
 }
 
-export default NavTabs;
+export default Footer;
