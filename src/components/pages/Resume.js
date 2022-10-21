adfc34467af373cc8e01060e1fd264bddd254931
SG_Demi <stevengoldbergm@gmail.com>
2022-10-20 20:45:19 -0500

    Added a downloadable rezoom

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
+// const downloadResume = () => {
+//   console.log('Write some code')
+
+//   const resume = require('../../Assets/rezoom.txt')
+
+//   const file = new Blob(resume)
+
+//   const a = document.createElement("a")
+//   a.href = URL.createObjectURL(file)
+//   a.download = "rezoom.txt"
+//   a.click()
+// }
+
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume</h1>
-      <p>
-        This is where I'll put my professional Resume as a dowloadable file.
-      </p>
+    <div className='container p-5 d-flex flex-column justify-content-center align-items-center'>
+      <a href={require("../../Assets/rezoom.txt")} download="rezoom.txt" className="col-md-5">
+      <button 
+        className="btn btn-primary " 
+        // value="download" 
+        // download="myfile" 
+        href={require("../../Assets/rezoom.txt")}>
+          Download a copy of my current resumé
+      </button>
+      </a>
+
+
     </div>
   );
 }
