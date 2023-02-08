2e2d5565777cff4ac70d8d979e904a82413b4801
SG_Demi <stevengoldbergm@gmail.com>
2023-02-08 03:47:05 -0600

    Fixed issue with footer not sticking to bottom of page at certain sizes

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -33,7 +33,7 @@ export default function PortfolioContainer() {
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
-    <div>
+    <div className='flex-wrapper bg-secondary'>
       {/* We are passing the currentPage from state and the function to update it */}
       <Header currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
