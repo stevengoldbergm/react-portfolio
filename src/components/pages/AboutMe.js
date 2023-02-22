faa4e63b7b4022982aeda184d760fd70672e01ad
SG_Demi <stevengoldbergm@gmail.com>
2023-02-22 02:02:30 -0600

    Updated About Me text, element order, and added different justify mode for better spacing

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,40 +1,47 @@
-import React from 'react';
-import avatar from '../../assets/imgs/Headshot_Basic.jpg';
+import avatar from "../../assets/imgs/Headshot_Basic-4.png";
 
 export default function About() {
   return (
-    <div className="container d-flex flex-column flex-md-row col-12 col-md-10 col-lg-8 p-3 justify-content-between align-items-center ">
-      <img src={avatar} className="d-md-none rounded-circle shadow bg-dark img-thumbnail col-4" width="25%" height="auto" style={{ maxWidth: "200px" }} alt="Current Headshot" />
+    <>
+      <div className="container d-flex flex-column flex-lg-row col-12 col-md-10 col-lg-8 p-3 justify-content-around align-items-center mt-3">
+        <img
+          src={avatar}
+          className="d-md-none rounded-circle shadow bg-dark img-thumbnail col-4"
+          alt="Current Headshot"
+        />
 
-      <img src={avatar} className="d-none d-md-block shadow bg-dark img-thumbnail col-4" width="25%" height="auto" style={{ maxWidth: "25vw" }} alt="Current Headshot" />
-      
-      <article className="col-md-7 mt-1 p-4 rounded d-flex flex-column">
-        {/* <h5 className=''>
-          About Me! 
-        </h5> */}
-        <br/>
-        Hi, I'm Steve, and I am a certified full-stack web-developer. 
-        <br/>
-        <br/>
-        I have a varied professional background, a bachelor's degree in psychology, and nearly a decade's worth of experience leading teams and projects in imaging operations for financial institutions. I'm passionate about continuing to learn and improve myself, and I love helping others. When I'm not working, I like to read fantasy novels and run TTRPGs for my friends.
-        <br></br>
-        <br></br> 
-        Please take a look at my work, and feel free to contact me if you're looking for a developer with great communication skills, a growth mindset, and a strong work ethic!
-        <br/>
-        <br/>
+        <img
+          src={avatar}
+          className="d-none d-md-block shadow bg-dark img-thumbnail col-4"
+          alt="Current Headshot"
+        />
 
-        <div className='mt-3 col-10 align-self-center d-flex justify-content-center'>
-          <a href={require("../../assets/StevenGoldbergResume_2023.pdf")} download="StevenGoldbergResume_2023.pdf" className="col-md-5 d-flex justify-content-center align-items-center">
-            <button class="btn btn-primary" href="/react-portfolio/static/media/StevenGoldbergResume_2023.7f5db7356c026c073ef4.pdf">
+        <article className=" p-4 rounded d-flex flex-column">
+          Hi, I'm Steve, and I am a certified full-stack web-developer.
+          <br />
+          <br />
+          I also have a bachelor's degree in psychology and nearly a decade's worth of experience leading teams and projects in imaging operations for financial institutions. I'm passionate about continuing to learn and improve myself, and I love helping others. When I'm not working, I like to read fantasy novels and run TTRPGs for my friends.
+          <br />
+          <br />
+          Please take a look at my work, and feel free to contact me if you're
+          looking for a developer with great communication skills, a growth
+          mindset, and a strong work ethic!
+        </article>
+      </div>
+      <div className="mt-3 col-10 align-self-center d-flex justify-content-center">
+        <a
+          href={require("../../assets/StevenGoldbergResume_2023.pdf")}
+          download="StevenGoldbergResume_2023.pdf"
+          className="col-md-8 d-flex justify-content-center align-items-center"
+        >
+          <button
+            class="btn btn-primary"
+            href={require("../../assets/StevenGoldbergResume_2023.pdf")}
+          >
             Check out my resumé
-            </button>
-          </a>
-        </div>
-        
-
-      </article>
-
-
-    </div>
+          </button>
+        </a>
+      </div>
+    </>
   );
 }
